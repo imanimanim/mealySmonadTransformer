82f39a1fd1e657c789c6440be0358b85f277179f
imanimanim <118016823+imanimanim@users.noreply.github.com>
2026-06-18 15:58:16 +0200

    Update MealyMT.hs

--- MealyMT.hs
+++ MealyMT.hs
@@ -10,7 +10,7 @@ import Control.Monad.Trans.State
 import Control.Monad.Trans.Maybe
 import Control.Monad.Trans.Accum
 import Control.Monad.Trans.Except
-import Control.Monad.Trans.Writer.Strict -- strict notwendig?
+import Control.Monad.Trans.Writer.Strict
 import Control.Monad.Trans.Select
 import Data.Functor.Identity
 import qualified Control.Monad.Trans.Class as L
@@ -70,6 +70,14 @@ instance Monoid w => MonadHandler (StateT s (AccumT w Identity)) s where
   unmonad s _ m =
     let ((o,s'),_) = runStAc m s mempty -- empty akkum für jeden typen
     in (s',o)
+-- verwirft akkumulierten Wert w
+{-
+ let ((o,_),w) = runStAc m s []
+    in (case w of
+          [] -> s
+          xs -> last xs
+       , o)
+-}
 
 --
 
@@ -115,7 +123,7 @@ runExSt m s = runIdentity (runStateT (runExceptT m) s)
 instance MonadHandler (ExceptT e (StateT s Identity)) s where
   unmonad s def m =
     case runExSt m s of
-      (Left _, _) -> (s', def)
+      (Left _, _) -> (s, def)
       (Right o,s') -> (s', o)
 
 --
@@ -130,37 +138,7 @@ runStWr m s = runIdentity (runWriterT (runStateT m s))
 
 instance Monoid w => MonadHandler (StateT s (WriterT w Identity)) s where
   unmonad s _ m =
-    let ((o,s'), _) = runStWr m s
-    in (s', o)
-
---
-
-instance Monoid w => MealyState s (WriterT w (StateT s Identity)) where
-  getM = L.lift get
-  putM = L.lift . put
-  stateM = L.lift . state
-
-runWrSt :: WriterT w (StateT s Identity) a -> s -> ((a,w), s)
-runWrSt m s = runIdentity (runStateT (runWriterT m) s)
-
-instance Monoid w => MonadHandler (WriterT w (StateT s Identity)) s where
-  unmonad s _ m =
-    let ((o,_), s') = runWrSt m s
-    in (s', o)
-
---
-
-instance Monoid w => MealyState s (StateT s (WriterT w Identity)) where
-  getM = get
-  putM = put
-  stateM = state
-
-runStWr :: StateT s (WriterT w Identity) a -> s -> ((a,s), w)
-runStWr m s = runIdentity (runWriterT (runStateT m s))
-
-instance Monoid w => MonadHandler (StateT s (WriterT w Identity)) s where
-  unmonad s _ m =
-    let ((o,s'), _) = runStWr m s
+    let ((o,s'), _) = runStWr m s -- was soll mit dem log output passieren
     in (s', o)
 
 --
