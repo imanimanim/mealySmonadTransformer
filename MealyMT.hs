da8e408c972984e07c5f2fdcd08ae34b412bc5cf
imanimanim <118016823+imanimanim@users.noreply.github.com>
2026-08-12 15:14:02 +0200

    Update MealyMT.hs

--- MealyMT.hs
+++ MealyMT.hs
@@ -3,106 +3,104 @@
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE FunctionalDependencies #-}
 
-module Example.MealyST where
+module Example.MealyT where
 
 import Clash.Prelude
-import Control.Monad.Trans.State
+import Control.Monad.State.Class (MonadState(..))
+import Control.Monad.Trans.State hiding (get, put, state)
 import Control.Monad.Trans.Maybe
 import Control.Monad.Trans.Accum
 import Control.Monad.Trans.Except
 import Control.Monad.Trans.Writer.Strict
-import Control.Monad.Trans.Select
 import Data.Functor.Identity
 import qualified Control.Monad.Trans.Class as L
-
-class Monad m => MealyState s m where
-  getM :: m s
-  putM :: s -> m ()
-  stateM :: (s -> (a,s)) -> m a
 
 class Monad m => MonadHandler m s | m -> s where
   unmonad :: s -> o -> m o -> (s, o) -- s is initial state, o default output
 
---
+-- ============================================================
+-- StateT s (MaybeT Identity)
+-- ============================================================
 
-instance MealyState s (StateT s (MaybeT Identity)) where
-  getM = get
-  putM = put
-  stateM = state
-
-runStMa :: StateT s (MaybeT Identity) a -> s -> Maybe (a, s)
-runStMa m s = runIdentity (runMaybeT (runStateT m s))
+runStMa
+  :: StateT s (MaybeT Identity) a
+  -> s
+  -> Maybe (a, s)
+runStMa m s =
+  runIdentity (runMaybeT (runStateT m s))
 
 instance MonadHandler (StateT s (MaybeT Identity)) s where
   unmonad s def m =
     case runStMa m s of
-      Nothing      -> (s, def) -- rollback auf alten state
+      Nothing      -> (s, def) -- rollback to old state
       Just (o,s')  -> (s', o)
 
---
+-- ============================================================
+-- MaybeT (StateT s Identity)
+-- ============================================================
 
-instance MealyState s (MaybeT (StateT s Identity)) where
-  getM = L.lift get
-  putM = L.lift . put
-  stateM = L.lift . state
-
-runMaSt :: MaybeT (StateT s Identity) a -> s -> (Maybe a, s)
-runMaSt m s = runIdentity (runStateT (runMaybeT m) s)
+runMaSt
+  :: MaybeT (StateT s Identity) a
+  -> s
+  -> (Maybe a, s)
+runMaSt m s =
+  runIdentity (runStateT (runMaybeT m) s)
 
 instance MonadHandler (MaybeT (StateT s Identity)) s where
   unmonad s def m =
     let (res, s') = runMaSt m s
     in case res of
-         Nothing -> (s', def) -- neuer state bleibt trotz fehler
+         Nothing -> (s', def) -- new state stays despite error
          Just o  -> (s', o)
 
---
+-- ============================================================
+-- StateT s (AccumT w Identity)
+-- ============================================================
 
-instance Monoid w => MealyState s (StateT s (AccumT w Identity)) where
-  getM = get
-  putM = put
-  stateM = state
+runStAc
+  :: Monoid w
+  => StateT s (AccumT w Identity) a
+  -> s
+  -> w
+  -> ((a,s), w) -- final value, accum
+runStAc m s w0 =
+  runIdentity (runAccumT (runStateT m s) w0) -- w0 is staring value of the accum
 
-runStAc :: StateT s (AccumT w Identity) a -> s -> w -> ((a,s), w) -- endwert, akkum
-runStAc m s w0 = runIdentity (runAccumT (runStateT m s) w0) -- w0 startwert akkum
-
-instance Monoid w => MonadHandler (StateT s (AccumT w Identity)) s where
+instance Monoid w
+    => MonadHandler (StateT s (AccumT w Identity)) s where
   unmonad s _ m =
-    let ((o,s'),_) = runStAc m s mempty -- empty akkum für jeden typen
+    let ((o,s'),_) = runStAc m s mempty -- empty accum for every type
     in (s',o)
--- verwirft akkumulierten Wert w
-{-
- let ((o,_),w) = runStAc m s []
-    in (case w of
-          [] -> s
-          xs -> last xs
-       , o)
--}
 
---
+-- ============================================================
+-- AccumT w (StateT s Identity)
+-- ============================================================
 
-instance Monoid w => MealyState s (AccumT w (StateT s Identity)) where
-  getM = L.lift get
-  putM = L.lift . put
-  stateM = L.lift . state
+runAcSt
+  :: Monoid w
+  => AccumT w (StateT s Identity) a
+  -> s
+  -> w
+  -> ((a,w),s)
+runAcSt m s w0 =
+  runIdentity (runStateT (runAccumT m w0) s)
 
-runAcSt :: AccumT w (StateT s Identity) a -> s -> w -> ((a,w),s)
-runAcSt m s w0 = runIdentity (runStateT (runAccumT m w0) s)
-
-instance Monoid w => MonadHandler (AccumT w (StateT s Identity)) s where
+instance Monoid w
+    => MonadHandler (AccumT w (StateT s Identity)) s where
   unmonad s _ m =
     let ((o,_),s') = runAcSt m s mempty
     in (s', o)
 
---
+-- ============================================================
+-- StateT s (ExceptT e Identity)
+-- ============================================================
 
-instance MealyState s (StateT s (ExceptT e Identity)) where
-  getM = get
-  putM = put
-  stateM = state
-
-runStEx :: StateT s (ExceptT e Identity) a -> s -> Either e (a,s)
-runStEx m s = runIdentity (runExceptT (runStateT m s))
+runStEx
+  :: StateT s (ExceptT e Identity) a
+  -> s
+  -> Either e (a,s)
+runStEx m s =
+    runIdentity (runExceptT (runStateT m s))
 
 instance MonadHandler (StateT s (ExceptT e Identity)) s where
   unmonad s def m =
@@ -110,66 +108,76 @@ instance MonadHandler (StateT s (ExceptT e Identity)) s where
       Left _ -> (s, def)
       Right (o,s') -> (s', o)
 
---
+-- ============================================================
+-- ExceptT e (StateT s Identity)
+-- ============================================================
 
-instance MealyState s (ExceptT s (StateT s Identity)) where
-  getM = L.lift get
-  putM = L.lift . put
-  stateM = L.lift . state
-
-runExSt :: ExceptT e (StateT s Identity) a -> s -> (Either e a, s)
-runExSt m s = runIdentity (runStateT (runExceptT m) s)
+runExSt
+  :: ExceptT e (StateT s Identity) a
+  -> s
+  -> (Either e a, s)
+runExSt m s =
+    runIdentity (runStateT (runExceptT m) s)
 
 instance MonadHandler (ExceptT e (StateT s Identity)) s where
   unmonad s def m =
     case runExSt m s of
-      (Left _, _) -> (s, def)
+      (Left _, _) -> (s, def) -- rollback
       (Right o,s') -> (s', o)
 
---
+-- ============================================================
+-- StateT s (WriterT w Identity)
+-- ============================================================
 
-instance Monoid w => MealyState s (StateT s (WriterT w Identity)) where
-  getM = get
-  putM = put
-  stateM = state
+runStWr
+  :: Monoid w
+  => StateT s (WriterT w Identity) a
+  -> s
+  -> ((a,s), w)
+runStWr m s =
+  runIdentity (runWriterT (runStateT m s))
 
-runStWr :: StateT s (WriterT w Identity) a -> s -> ((a,s), w)
-runStWr m s = runIdentity (runWriterT (runStateT m s))
-
-instance Monoid w => MonadHandler (StateT s (WriterT w Identity)) s where
+instance Monoid w
+    => MonadHandler (StateT s (WriterT w Identity)) s where
   unmonad s _ m =
-    let ((o,s'), _) = runStWr m s -- was soll mit dem log output passieren
+    let ((o,s'), _) = runStWr m s
     in (s', o)
 
---
+-- ============================================================
+-- WriterT w (StateT s Identity)
+-- ============================================================
 
-instance Monoid w => MealyState s (WriterT w (StateT s Identity)) where
-  getM = L.lift get
-  putM = L.lift . put
-  stateM = L.lift . state
+runWrSt
+  :: Monoid w
+  => WriterT w (StateT s Identity) a
+  -> s
+  -> ((a,w), s)
+runWrSt m s =
+  runIdentity (runStateT (runWriterT m) s)
 
-runWrSt :: WriterT w (StateT s Identity) a -> s -> ((a,w), s)
-runWrSt m s = runIdentity (runStateT (runWriterT m) s)
-
-instance Monoid w => MonadHandler (WriterT w (StateT s Identity)) s where
+instance Monoid w
+    => MonadHandler (WriterT w (StateT s Identity)) s where
   unmonad s _ m =
     let ((o,_), s') = runWrSt m s
     in (s', o)
 
---
+-- ============================================================
+-- Mealy machine
+-- ============================================================
 
-mealyST
+mealyT
   :: MonadHandler m s
   => o
   -> (i -> m o)
   -> s
   -> i
   -> (s, o)
-mealyST def f s i =
+mealyT def f s i =
   unmonad s def (f i)
 
-
--------------------- tests --------------------
+-- ============================================================
+-- Tests
+-- ============================================================
 
 fSM :: Int -> StateT Int (MaybeT Identity) Int
 fSM x = do
@@ -180,7 +188,7 @@ fSM x = do
   if x < 0
     then L.lift (MaybeT (pure Nothing))
     else pure s'
--- simulate @System (mealy (mealyyMT (999 :: Int) fSM) 0) [1,2,3,-1,5]
+-- simulate @System (mealy (mealyT (999 :: Int) fSM) 0) [1,2,3,-1,5]
 -- [1,3,6,999,11] nach dem fehler wird wieder die 6 als state genommen
 
 fMS :: Int -> MaybeT (StateT Int Identity) Int
